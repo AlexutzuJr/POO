e149e99930bafea0094d5be95f5277cc89b7b077
AlexutzuJr <107935470+AlexutzuJr@users.noreply.github.com>
2023-08-06 12:05:53 +0300

    Update 1.9.cpp

--- Lab 1 (clase & obiecte)/1.9.cpp
+++ Lab 1 (clase & obiecte)/1.9.cpp
@@ -1,5 +1,5 @@
-/* Să se scrie o clasă Carte cu următoarele date membre: titlu, nume_autor, an_lansare, nr_pagini, pret. Se vor instanția obiecte din această clasă folosind un vector cu alocare dinamică de memorie. Se citește de 
-la tastatură numărul de cărți pe care dorim să îl folosim. Ulterior, pentru fiecare carte se vor citi, de pe rânduri diferite datele membre în ordinea enunțată mai sus.
+/* Să se scrie o clasă Carte cu următoarele date membre: titlu, nume_autor, an_lansare, nr_pagini, pret. Se vor instanția obiecte din această clasă folosind un vector cu alocare dinamică de memorie. Se citește de la 
+tastatură numărul de cărți pe care dorim să îl folosim. Ulterior, pentru fiecare carte se vor citi, de pe rânduri diferite datele membre în ordinea enunțată mai sus.
 
 Se va citi o comandă de la tastatură. În funcție de valoarea acesteia se vor executa diferite acțiuni:
 1. Se citește de la tastatură un nume de autor. Se afișează titlurile tuturor cărților scrise de acest autor, separate de virgulă și spațiu. Pentru simplitate, ultimul titlu va fi, de asemenea, urmat de virgulă și 

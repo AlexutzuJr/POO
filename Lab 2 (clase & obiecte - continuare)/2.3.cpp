52dfc287ad21b068be49184cbdec36df88a2b034
AlexutzuJr <107935470+AlexutzuJr@users.noreply.github.com>
2023-08-06 12:19:21 +0300

    Update 2.3.cpp

--- Lab 2 (clase & obiecte - continuare)/2.3.cpp
+++ Lab 2 (clase & obiecte - continuare)/2.3.cpp
@@ -1,14 +1,17 @@
 /* Să se scrie o clasă Pantof cu datele membre stil și mărime. Suplimentar, aceasta va conține o dată membră de tip întreg cu rolul de contor, ce va reține numărul de obiecte create, și o dată membră care reține numărul 
-maxim de obiecte ce pot fi create din această clasă (se presupune că sunt în ediție limitată). Dacă la tentativa de instanțiere a unui obiect se constată că s-a atins deja numărul maxim de obiecte ce pot fi instanțiate se va
-afișa pe ecran mesajul “S-a atins pragul maxim din editia limitata!”. Toate inițializările din constructori vor fi făcute cu ajutorul listei de inițializare.
+maxim de obiecte ce pot fi create din această clasă (se presupune că sunt în ediție limitată). Dacă la tentativa de instanțiere a unui obiect se constată că s-a atins deja numărul maxim de obiecte ce pot fi instanțiate 
+se va afișa pe ecran mesajul “S-a atins pragul maxim din editia limitata!”. Toate inițializările din constructori vor fi făcute cu ajutorul listei de inițializare.
 În programul principal se vor citi de la tastatură, de pe linii diferite, următoarele date:
--un întreg m, reprezentând numărul maxim de Pantofi pe care îi putem instanția;
--un întreg n, reprezentând numărul de obiecte pe care le vom instanția;
--n perechi de date de tipul std::string și int, reprezentând stilul și mărimea, câte o pereche pe fiecare rând. Stilul este reprezentat de un singur cuvânt.
+1. un întreg m, reprezentând numărul maxim de Pantofi pe care îi putem instanția;
+2. un întreg n, reprezentând numărul de obiecte pe care le vom instanția;
+3. n perechi de date de tipul std::string și int, reprezentând stilul și mărimea, câte o pereche pe fiecare rând. Stilul este reprezentat de un singur cuvânt.
+
 După fiecare instanțiere se vor afișa pe ecran stilul și marimea, separate de un spațiu și urmate de caracterul newline.
+
 Extensie: separați declarația clasei și definițiile funcțiilor membre în fișiere diferite.
 Extensie 2: cele n instanțe ale clasei Pantof vor fi reținute sub forma unui vector static de obiecte.
 Extensie 3: cele n instanțe ale clasei Pantof vor fi reținute sub forma unui vector dinamic de obiecte. */
+
 
 #include <iostream>
 #include <limits>

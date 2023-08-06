111221783d041fee5a0ed35c7e3b8f6c7089ba17
AlexutzuJr <107935470+AlexutzuJr@users.noreply.github.com>
2023-08-06 12:50:30 +0300

    Update 5.1.cpp

--- Lab 5 (supraincarcarea operatorilor)/5.1.cpp
+++ Lab 5 (supraincarcarea operatorilor)/5.1.cpp
@@ -1,14 +1,17 @@
 /* Să se scrie un program care execută operațiile de adunare, scădere și înmulțire între două matrice. Operația se va alege în funcție de o valoare de test citită de la tastatură.
+
 Se citesc de la tastatură:
 - doi întregi, m și n, reprezentând dimensiunile primei matrice
 - m x n întregi, reprezentând elementele primei matrice
 - doi întregi, q și p, reprezentând dimensiunile celei de-a doua matrice
 - q x p întregi, reprezentând elementele celei de-a doua matrice
 - un întreg, test, reprezentând valoarea în funcție de care se execută diverse funcții:
+
 1. adunare matrice - se afișează pe ecran elementele matricei obținută prin adunarea celor două matrice de intrare.
 2. scădere matrice -  se afișează pe ecran elementele matricei obținută prin scăderea celei de-a doua matrice din prima matrice.
 3. înmulțire matrice - se afișează pe ecran elementele matricei obținută prin înmulțirea primei matrice cu cea de-a doua matrice. */
-    
+
+
 #include <stdio.h>
 #include <iostream>
 

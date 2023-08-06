1a4c9bd31fc57e6d290277e7464d2c95846d317d
AlexutzuJr <107935470+AlexutzuJr@users.noreply.github.com>
2023-08-06 12:17:08 +0300

    Update 2.1.cpp

--- Lab 2 (clase & obiecte - continuare)/2.1.cpp
+++ Lab 2 (clase & obiecte - continuare)/2.1.cpp
@@ -1,6 +1,7 @@
-/* Să se scrie două clase distincte, Vector și Matrice care să conțină drept date membre un vector de maxim 20 de întregi, respectiv un tablou bidimensional, de dimensiune maximă 20 x 20. Se vor scrie constructorii aferenți celor 2 clase și o funcție membră de afișare 
-a elementelor (cel puțin pentru clasa Vector). Se va scrie o funcție de tip friend care să execute înmulțirea dintre un Vector și o Matrice. Funcția va verifica mai întâi dacă operația poate avea loc (numărul de coloane ale vectorului să coincidă cu numărul de linii 
-ale matricei). În caz contrar, va afișa mesajul “EROARE” pe ecran. Vectorul este considerat de tipul linie. Toate inițializările din constructori vor fi făcute cu ajutorul listei de inițializare.
+/* Să se scrie două clase distincte, Vector și Matrice care să conțină drept date membre un vector de maxim 20 de întregi, respectiv un tablou bidimensional, de dimensiune maximă 20 x 20. Se vor scrie constructorii 
+aferenți celor 2 clase și o funcție membră de afișare a elementelor (cel puțin pentru clasa Vector). Se va scrie o funcție de tip friend care să execute înmulțirea dintre un Vector și o Matrice. Funcția va verifica mai 
+întâi dacă operația poate avea loc (numărul de coloane ale vectorului să coincidă cu numărul de linii ale matricei). În caz contrar, va afișa mesajul “EROARE” pe ecran. Vectorul este considerat de tipul linie. Toate 
+inițializările din constructori vor fi făcute cu ajutorul listei de inițializare.
 Se citesc de la tastatură, de pe linii diferite, următoarele date:
 1. un întreg n, reprezentând numărul de elemente ale vectorului;
 2. n valori întregi, reprezentând valorile vectorului;
@@ -11,7 +12,7 @@ Se va afișa pe ecran rezultatul obținut în urma înmulțirii vectorului cu ma
 
 Extensie: funcția friend va întoarce în programul apelant un obiect de tipul Vector drept rezultat.
 Extensie 2: atât Vectorul, cât și Matricea, vor stoca valorile în mod dinamic.
-Extensie 3: separați declarațiile claselor și definițiile funcțiilor membre în fișiere diferite. Notă: trebuie avute în vedere declarațiile în avans (forward declaration) ale claselor și a funcției friend - discutăm împreună. */
+Extensie 3: separați declarațiile claselor și definițiile funcțiilor membre în fișiere diferite. Notă: trebuie avute în vedere declarațiile în avans (forward declaration) ale claselor și a funcției friend. */
 
 
 #include <iostream>
